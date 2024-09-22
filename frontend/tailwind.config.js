044b58bb0360d45b3f12aac0fc6f893af379d0ed
Daikenz <daikenz.metaverse@gmail.com>
2024-09-22 22:08:54 +0530

    Fix - No Idea of these files

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,5 +1,7 @@
 /** @type {import('tailwindcss').Config} */
-export default {
+const withMT = require("@material-tailwind/react/utils/withMT");
+
+export default withMT({
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {
@@ -17,5 +19,5 @@ export default {
     
   },
   plugins: [],
-}
+});
 
